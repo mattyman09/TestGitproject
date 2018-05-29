4c6d54bf94b4976c358258a3ad89f9f9e6cd1332
Matthew Pickup <s4821175@bournemouth.ac.uk>
2018-05-29 06:45:35 +0100

    finished machine collisions and added sound

--- Assets/teleport.cs
+++ Assets/teleport.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class teleport : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+public class teleport : MonoBehaviour
+{
+
+    public float speed = 0.9f;
+    public Vector3 startpos;
+    public Vector3 endpos;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Teleporting Sphere
+
+    void OnCollisionEnter(Collision col)
+
+    {
+        if (col.gameObject.name == "teleport_platform") //When the "Sphere" object collides with "teleport_platform" object teleports to location
+        {
+            transform.position = Vector3.Lerp(endpos, startpos, speed * Time.deltaTime);
+        }
+    }
 }
